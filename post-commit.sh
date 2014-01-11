7442bbff127ea79e7bce5e7bfb77fc27c9a1036e
Shichao An <shichao.an@nyu.edu>
2014-01-10 22:46:47 -0500

    Post-commit log on remote host

--- post-commit.sh
+++ post-commit.sh
@@ -8,8 +8,10 @@ source remote.sh
 
 # Run commands on the remote host
 session_name="shichao-an.info"
+logfile="post-commit.log"
 ssh $remote_username@$remote_hostname "if screen -list | grep -E '\<[0-9]+\.$session_name\>' >/dev/null;
 then echo -n ''; else screen -dmS $session_name; fi"
-ssh -t $remote_username@$remote_hostname "screen -S $session_name -p 0 -X stuff 'cd shichao-an.info; make html &> post-commit.txt
+ssh -t $remote_username@$remote_hostname \
+"screen -S $session_name -p 0 -X stuff 'cd shichao-an.info; date >> $logfile; make html &>> $logfile
 '
 "
