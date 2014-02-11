abe99f1667e364c6a742b28d1a7a64b6f9a67c8a
Shichao An <shichao.an@nyu.edu>
2014-02-10 20:47:34 -0500

    Updated local.sh

--- local.sh
+++ local.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# For build
+sphinx-build -a -b html -d build/doctrees source build/html
+
 # Sync all files under build/html to another project directory
 rsync -avz build/html/ ../shichao-an.github.io
