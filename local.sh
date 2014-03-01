5dba8eb8beead38ffb4e8618022762ea2fbb6139
Shichao An <shichao.an@nyu.edu>
2014-02-28 20:10:19 -0500

    Modified local.sh to handle error

--- local.sh
+++ local.sh
@@ -2,6 +2,10 @@
 
 # For build
 sphinx-build -a -b html -d build/doctrees source build/html
+if [ "$?" -ne 0 ]
+then
+    exit 1
+fi
 
 # Sync all files under build/html to another project directory
 rsync -avz build/html/ ../shichao-an.github.io
